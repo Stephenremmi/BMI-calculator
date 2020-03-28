06dbfb52ddd850c365ce57f51dca6d7db726b1f6
stephen remmi <stephenremmi@gmail.com>
2020-03-28 13:43:09 +0300

    Change link

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var divide = function(number1, number2){
+return number1 / number2;};
+var number1 = parseInt(prompt("Weight(kg) :"));
+var number2 = parseInt(prompt("Height(cm) :"));
+var result = divide(number1, number2);
+alert(result);
